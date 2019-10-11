24ad6cd686f6e1a6ed465b5a0ac64b51e6b4e043
xuwang <wx2700317@users.noreply.github.com>
2019-10-11 19:16:48 +0800

    booksystem01

---
+++ PowerSystem/src/main/java/com/power1/PowerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.power1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerSystemApplication.class, args);
+	}
+
+}
